072882b952fd16d423eea41ca77a3a3d89467a74
ksanchezcld <ing.kksanchez@gmail.com>
2018-12-04 18:14:54 -0400

    Second Update

--- clima/clima.js
+++ clima/clima.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const getClima = async(lat, lng) => {
 
-    let resp = await axios.get(`api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&appid=437f25ce93c9cc818f4a9afabe8d63eb`)
+    let resp = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&appid=437f25ce93c9cc818f4a9afabe8d63eb`)
 
     return resp.data.main.temp;
 
